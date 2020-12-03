1cee3890a800dc5fd611421d59e5bba3f35d1903
widari1212 <widari1212.widari415@gmail.com>
2020-12-03 10:34:08 +0700

    added=>laravel

---
+++ Sprint7/coba_laravel/app/Model/Mahasiswa.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $table = 'mahasiswa'; //simpan nama table disini
+}
---
+++ Sprint7/coba_laravel/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('container')
+  <div class="container">
+    <h1>Ini Adalah Halaman Utama</h1>
+</div>
+@endsection
+
